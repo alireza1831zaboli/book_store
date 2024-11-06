4d088b8d701b6c36de17bb32445e8979334d6241
Alireza <alireza18z31@gmail.com>
2024-11-06 20:37:33 +0330

    feat: adding postgresql database as project database

--- library/library/settings.py
+++ library/library/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = "library.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "library_db",
+        "USER": "postgres",
+        "PASSWORD": "3456",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
